3c10d348b068c22fc8397c01c0d87acc191ae9a2
Muhammad Taha <taha_khan198923@yahoo.com>
2017-03-06 15:38:21 +0500

    added slider and working properly

--- AwesomeProject/index.android.js
+++ AwesomeProject/index.android.js
@@ -13,11 +13,42 @@ import {
   Text,
   View
 } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+
+var styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
 
 export default class AwesomeProject extends Component {
   render() {
     return (
       <Container>
+      
                 <Header>
                     <Left>
                         <Button transparent>
@@ -30,6 +61,17 @@ export default class AwesomeProject extends Component {
                     <Right />
                 </Header>
                   <Content>
+                          <Swiper style={styles.wrapper} showsButtons={true} autoplay={true}>
+                          <View style={styles.slide1}>
+                          <Text style={styles.text}>Hello Swiper</Text>
+                          </View>
+                          <View style={styles.slide2}>
+                          <Text style={styles.text}>Beautiful</Text>
+                          </View>
+                          <View style={styles.slide3}>
+                          <Text style={styles.text}>And simple</Text>
+                          </View>
+                          </Swiper>
                     <Grid>
                         <Col style={{ backgroundColor: '#635DB7', height: 200 }}></Col>
                         <Col style={{ backgroundColor: '#00CE9F', height: 200  }}></Col>
